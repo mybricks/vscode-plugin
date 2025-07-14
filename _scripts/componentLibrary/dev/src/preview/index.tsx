1549ff688716d5385673a8bdaab3085efa8824a1
leon-js <42924541+leon-js@users.noreply.github.com>
2025-07-14 13:10:22 +0800

    fix

--- _scripts/componentLibrary/dev/src/preview/index.tsx
+++ _scripts/componentLibrary/dev/src/preview/index.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import { call } from "@mybricks/plugin-connector-http";
 // @ts-ignore
 import { render as renderUI } from "@mybricks/render-web";
+import mergeWith from "lodash/mergeWith";
+import application from "@vscode/application";
 
 //准备编译的数据，结构为 {slot,script}，根据 toJSON 导出
 let json = localStorage.getItem("--preview--");
@@ -37,39 +39,41 @@ try {
   root.render(<Page />);
 })();
 
+const renderOptions = {
+  env: mergeWith({
+    i18n(text: string) {
+      return text;
+    },
+    callConnector: call,
+    vars: { // 环境变量
+      getQuery () { // 获取真实路由参数
+        return {};
+      },
+    },
+    ajax(url: string, opts: any) {
+      return new Promise((resolve, reject) => {
+        if (typeof url !== 'string') {
+          reject('url is undefined');
+        }
+        axios({url, ...opts}).then(resp => {
+          if (resp && resp.status === 200) {
+            resolve(resp.data);
+          } else {
+            reject(resp);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+      });
+    }
+  }, application?.com?.env || {})
+};
+
 function Page() {
   return (
     <div style={{width: '100%', height: '100%'}}>
       {
-        renderUI(json, {
-          env: {
-            i18n(text: string) {
-              return text;
-            },
-            callConnector: call,
-            vars: { // 环境变量
-              getQuery () { // 获取真实路由参数
-                return {};
-              },
-            },
-            ajax(url: string, opts: any) {
-              return new Promise((resolve, reject) => {
-                if (typeof url !== 'string') {
-                  reject('url is undefined');
-                }
-                axios({url, ...opts}).then(resp => {
-                  if (resp && resp.status === 200) {
-                    resolve(resp.data);
-                  } else {
-                    reject(resp);
-                  }
-                }).catch(error => {
-                  reject(error);
-                });
-              });
-            }
-          },
-        })
+        renderUI(json, renderOptions)
       }
     </div>
   );
